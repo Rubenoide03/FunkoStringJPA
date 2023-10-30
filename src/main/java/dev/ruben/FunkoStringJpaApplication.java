8b2ab7c4f9422347d39d85e655fea90464891b65
Rubenoide03 <rubenfernandezp03@gmail.com>
2023-10-30 21:35:40 +0100

    Initial commit

---
+++ src/main/java/dev/ruben/FunkoStringJpaApplication.java
@@ -0,0 +1,13 @@
+package dev.ruben;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunkoStringJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunkoStringJpaApplication.class, args);
+    }
+
+}
